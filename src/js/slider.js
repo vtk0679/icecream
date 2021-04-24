d27c7dab310f26f0a55ce3912119adc7993bf7d7
Vitaliy <vtk0679@gmail.com>
2021-04-24 23:14:22 +0300

    adds jquery

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,11 +1,10 @@
-$(function (){
-    
-    $('.reviews__slider').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  adaptiveHeight: true
-})
-
+$('.reviews__slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    arrows: false
 });
