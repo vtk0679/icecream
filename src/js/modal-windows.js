16e217684ac506a73d457a11439647af5d7f42c7
Serhii Tkachenko <x.ray868@gmail.com>
2021-04-27 16:24:14 +0300

    update all

--- src/js/modal-windows.js
+++ src/js/modal-windows.js
@@ -36,6 +36,15 @@
     const refs = {
         openModalBtns: document.querySelectorAll('[data-modal-team-open]'),
         closeModalBtn: document.querySelector('[data-modal-team-close]'),
+        closeModalBtnAll: document.querySelector('[data-close-all]'),
+        closeModalBtnContact: document.querySelector('[data-close-contact]'),
+        closeModalBtnAbout: document.querySelector('[data-close-about]'),
+        closeModalBtnHome: document.querySelector('[data-close-home]'),
+        closeModalBtnProducts: document.querySelector('[data-close-products]'),
+        closeModalBtnHow: document.querySelector('[data-close-how]'),
+        closeModalBtnGallery: document.querySelector('[data-close-gallery]'),
+        closeModalBtnAdvantages: document.querySelector('[data-close-advantages]'),
+        closeModalBtnFooter: document.querySelector('[data-close-footer]'),
         modal: document.querySelector('[data-modal-team]'),
     };
 
@@ -43,6 +52,15 @@
         button.addEventListener('click', toggleModal);
     });
     refs.closeModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtnAll.addEventListener('click', toggleModal);
+    refs.closeModalBtnContact.addEventListener('click', toggleModal);
+    refs.closeModalBtnAbout.addEventListener('click', toggleModal);
+    refs.closeModalBtnHome.addEventListener('click', toggleModal);
+    refs.closeModalBtnProducts.addEventListener('click', toggleModal);
+    refs.closeModalBtnHow.addEventListener('click', toggleModal);
+    refs.closeModalBtnGallery.addEventListener('click', toggleModal);
+    refs.closeModalBtnAdvantages.addEventListener('click', toggleModal);
+    refs.closeModalBtnFooter.addEventListener('click', toggleModal);
 
     function toggleModal() {
         document.body.classList.toggle('modal-open');
