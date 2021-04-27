55f9d1909c7811d37a00b9b821787df058e28d06
Vitaliy <vtk0679@gmail.com>
2021-04-27 13:40:25 +0300

    adds backdrop for menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,20 +3,14 @@ $(function() {
     
     function hideModals(){
 	    $(document.body).removeClass('modal-open');
-        $(".menu-burger").addClass("is-hidden");
+        $(".menu-burger__backdrop").addClass("is-hidden");
     }
 
      $(".header-list-modal li a").click(function() {   
          hideModals();
     })
 
-	$(document).on('click', function(e){
-		if (!(
-		($(e.target).parents('.menu-burger').length)
-		||	($(e.target).hasClass('menu-burger'))
-        ||	($(e.target).hasClass('button-burger__icon')))
-		) {
+	$(".menu-burger__backdrop").on('click', function(e){
 			hideModals();
-		}
 	});
 });
